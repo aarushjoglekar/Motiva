d0319ce1b8a6004943e5501e744e9876fd040317
aarushjoglekar <aarush.joglekar@gmail.com>
2026-05-31 22:50:40 -0700

    fix direction of tz joint, switch tz actuator from position to motor, scale actions in step

--- motiva/environment/environment.py
+++ motiva/environment/environment.py
@@ -4,47 +4,82 @@ import numpy as np
 
 class Environment:
     def __init__(self):
+        # instantiation
         self.model, self.data = self.initialize_models()
         self.viewer = None
-        self.forearm_ids = [mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_BODY, forearm_name) for forearm_name in ["lh_forearm", "rh_forearm"]]
         
-    def step(self, action=None):
-        if action is not None:
-            self.data.ctrl[:] = action
+        # joint/actuator ids
+        self.forearm_ids = [
+            mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_BODY, forearm_name)
+            for forearm_name in ["lh_forearm", "rh_forearm"]
+        ]
+
+        self.rz_actuator_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_ACTUATOR, "rh_A_forearm_tz")
+        self.lz_actuator_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_ACTUATOR, "lh_A_forearm_tz")
+        self.rz_joint_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_JOINT, "rh_forearm_tz")
+        self.lz_joint_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_JOINT, "lh_forearm_tz")
+
+        # gains for the tz joints in both arms
+        self.z_kP = 3
+        self.z_kD = 0.7
+        self.z_kS = 0.313
+
+        # range of actions
+        self.action_lows  = self.model.actuator_ctrlrange[:, 0]
+        self.action_highs = self.model.actuator_ctrlrange[:, 1]
+
+    # action is a list indexed by actuator id of position values from -1 to 1
+    # step will automatically scale based on each control range
+    def step(self, action: np.ndarray):
+        # scale actions
+        scaled_action = self.action_lows + (action + 1) * 0.5 * (self.action_highs - self.action_lows)
+
+        # pd position control
+        self.data.ctrl[:] = scaled_action
+
+        # position to force control for motors
+        r_target_pos = scaled_action[self.rz_actuator_id]
+        r_current_pos = self.data.qpos[self.rz_joint_id]
+        r_current_vel = self.data.qvel[self.rz_joint_id]
+
+        l_target_pos = scaled_action[self.lz_actuator_id]
+        l_current_pos = self.data.qpos[self.lz_joint_id]
+        l_current_vel = self.data.qvel[self.lz_joint_id]
+
+        self.data.ctrl[self.rz_actuator_id] = self.z_kP * (r_target_pos - r_current_pos) - self.z_kD * r_current_vel + self.z_kS
+        self.data.ctrl[self.lz_actuator_id] = self.z_kP * (l_target_pos - l_current_pos) - self.z_kD * l_current_vel + self.z_kS
+
         mujoco.mj_step(self.model, self.data)
         return self.get_obs()
-    
+
     def get_obs(self):
         # qpos -> all joint positions (each hand + piano keys)
         # xpos -> forearm positions
-        return np.concatenate(
-            (self.data.qpos, self.data.xpos[self.forearm_ids].ravel())
-        )
-                
+        return np.concatenate((self.data.qpos, self.data.xpos[self.forearm_ids].ravel()))
+
     def render(self):
         if self.viewer is None:
             self.viewer = mujoco.viewer.launch_passive(
-                self.model, self.data,
-                show_left_ui=False, show_right_ui=False
+                self.model, self.data, show_left_ui=False, show_right_ui=True
             )
         self.viewer.sync()
-      
+
     def reset(self):
         mujoco.mj_resetData(self.model, self.data)
         mujoco.mj_forward(self.model, self.data)
-        
+
     def viewer_running(self):
         return self.viewer is None or self.viewer.is_running()
-                
+
     def __enter__(self):
         return self
-    
+
     def __exit__(self, *args):
         if self.viewer is not None:
             self.viewer.close()
             self.viewer = None
-                
-    def initialize_models(self, hover_offset=0.12, forward_offset=0.4):
+
+    def initialize_models(self, hover_offset:float=0.12, forward_offset:float=0.4):
         DIR = os.path.dirname(os.path.abspath(__file__))
         model = mujoco.MjModel.from_xml_path(os.path.join(DIR, "models/world.xml"))
         data = mujoco.MjData(model)
@@ -81,35 +116,35 @@ class Environment:
             forearm_z,
         ]
 
-        # ids for tx (world y axis)
-        rh_tx_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_JOINT, "rh_forearm_ty")
-        lh_tx_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_JOINT, "lh_forearm_ty")
+        # ids for ty
+        rh_ty_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_JOINT, "rh_forearm_ty")
+        lh_ty_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_JOINT, "lh_forearm_ty")
 
         # set the right hand range to the ends of the piano
-        model.jnt_range[rh_tx_id] = [
+        model.jnt_range[rh_ty_id] = [
             y_min - model.body_pos[rh_forearm_id][1],
             y_max - model.body_pos[rh_forearm_id][1],
         ]
 
         # set the left hand range to the ends of the piano
-        model.jnt_range[lh_tx_id] = [
+        model.jnt_range[lh_ty_id] = [
             y_min - model.body_pos[lh_forearm_id][1],
             y_max - model.body_pos[lh_forearm_id][1],
         ]
 
         # actuator ids
-        rh_tx_act_id = mujoco.mj_name2id(
+        rh_ty_act_id = mujoco.mj_name2id(
             model, mujoco.mjtObj.mjOBJ_ACTUATOR, "rh_A_forearm_ty"
         )
-        lh_tx_act_id = mujoco.mj_name2id(
+        lh_ty_act_id = mujoco.mj_name2id(
             model, mujoco.mjtObj.mjOBJ_ACTUATOR, "lh_A_forearm_ty"
         )
 
         # set control range of actuators to match joint range
-        model.actuator_ctrlrange[rh_tx_act_id] = model.jnt_range[rh_tx_id]
-        model.actuator_ctrlrange[lh_tx_act_id] = model.jnt_range[lh_tx_id]
+        model.actuator_ctrlrange[rh_ty_act_id] = model.jnt_range[rh_ty_id]
+        model.actuator_ctrlrange[lh_ty_act_id] = model.jnt_range[lh_ty_id]
 
         # initial forward pass to recompute positions from pre-updated positions
         mujoco.mj_forward(model, data)
-        
+
         return model, data
--- motiva/main.py
+++ motiva/main.py
@@ -1,6 +1,7 @@
 from environment.environment import Environment
+import numpy as np
 
 with Environment() as env:
     while env.viewer_running():
-        env.step()
+        env.step(action=np.zeros(46))
         env.render()
