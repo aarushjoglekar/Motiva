ff878a2b09afb24e08f5f3ac269b777040799bf0
aarushjoglekar <aarush.joglekar@gmail.com>
2026-06-08 13:52:09 -0700

    finish song testers

--- motiva/testers/testers.py
+++ motiva/testers/testers.py
@@ -7,3 +7,9 @@ def song_midi_representation(song: Song):
     filename = os.path.join(DIR, "song_midi_representation")
     os.makedirs(filename, exist_ok=True)
     mid.save(filename=f"{filename}/{song.name}.mid")
+
+def first_active_fingers(song: Song):
+    print(song.fingers_to_keys_data[:20])
+
+def sample_at_zero(song: Song):
+    print(song.sample_at(0))
