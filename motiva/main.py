53e8b191f55897c9c0245606a79f44d6cf66b34f
aarushjoglekar <aarush.joglekar@gmail.com>
2026-06-26 12:16:32 -0700

    time the full train loop + include prints for each episode

--- motiva/main.py
+++ motiva/main.py
@@ -13,8 +13,8 @@ MODEL_NAME = "motiva"
 
 # TRAINING SETTINGS
 TRAINING = True
-NUM_STEPS = 50
-VALIDATION_INTERVAL = 2
+NUM_STEPS = 50000
+VALIDATION_INTERVAL = 25000
 
 # TESTING SETTINGS
 SAVE_TO_MIDI = False
@@ -23,6 +23,7 @@ SAVE_TO_MIDI = False
 SONG = Song.from_txt(Song.SOMEWHERE_OVER_THE_RAINBOW)
 
 
+# returns total reward, avg actor loss, avg critic loss, avg log probs, avg alpha
 def run_episode(
     model: SAC_DROQ,
     env: Environment,
@@ -46,6 +47,11 @@ def run_episode(
         model.eval()
 
     steps = 0
+    sum_reward = 0
+    sum_actor_loss = 0
+    sum_critic_loss = 0
+    sum_log_probs = 0
+    sum_alpha = 0
 
     while True:
         action, _ = model.select_action(
@@ -54,10 +60,12 @@ def run_episode(
 
         next_observation, reward, truncated = env.step(action=action.detach().numpy())
         next_observation = torch.from_numpy(next_observation).float()
+
         steps += 1
+        sum_reward += reward
 
         if episode_type == EpisodeType.TRAINING:
-            model.update(
+            actor_loss, critic_loss, log_probs, alpha = model.update(
                 state=observation,
                 next_state=next_observation,
                 action=action,
@@ -65,16 +73,37 @@ def run_episode(
                 truncated=truncated,
             )
 
+            sum_actor_loss += actor_loss
+            sum_critic_loss += critic_loss
+            sum_log_probs += log_probs
+            sum_alpha += alpha
+
         observation = next_observation
 
         if on_step_end is not None:
             on_step_end()
 
         if truncated:
-            return steps, False
+            return (
+                sum_reward,
+                sum_actor_loss / steps,
+                sum_critic_loss / steps,
+                sum_log_probs / steps,
+                sum_alpha / steps,
+                steps,
+                False,
+            )
 
         if episode_type == EpisodeType.TEST and not env.viewer_running():
-            return steps, True
+            return (
+                sum_reward,
+                sum_actor_loss / steps,
+                sum_critic_loss / steps,
+                sum_log_probs / steps,
+                sum_alpha / steps,
+                steps,
+                True,
+            )
 
 
 def run_test(model: SAC_DROQ, env: Environment, model_path: str):
@@ -91,13 +120,16 @@ def run_test(model: SAC_DROQ, env: Environment, model_path: str):
         if sleep_time > 0:
             time.sleep(sleep_time)
 
-    _, closed_viewer = run_episode(
+    sum_reward, _, _, _, _, _, closed_viewer = run_episode(
         model=model,
         env=env,
         episode_type=EpisodeType.TEST,
         model_path=model_path,
         on_step_end=pace,
     )
+
+    print(f"Test Episode || Total Reward: {sum_reward}")
+
     if not closed_viewer:
         time.sleep(2)
 
@@ -108,8 +140,18 @@ def run_training(model: SAC_DROQ, env: Environment, model_path: str):
     num_steps = 0
     episode = 0
 
+    start_time = time.perf_counter()
+
     while num_steps < NUM_STEPS:
-        steps, _ = run_episode(
+        (
+            sum_reward,
+            avg_actor_loss,
+            avg_critic_loss,
+            avg_log_probs,
+            avg_alpha,
+            steps,
+            _,
+        ) = run_episode(
             model=model,
             env=env,
             episode_type=(
@@ -122,6 +164,12 @@ def run_training(model: SAC_DROQ, env: Environment, model_path: str):
 
         episode += 1
         num_steps += steps
+
+        print(
+            f"Episode: {episode} || Reward: {sum_reward} || Actor Loss: {avg_actor_loss} || Critic Loss: {avg_critic_loss} || Log Prob: {avg_log_probs} || Alpha: {avg_alpha} || Total Steps: {num_steps}"
+        )
+
+    print(f"Final Time: {time.perf_counter() - start_time}")
 
     model.save()
 
