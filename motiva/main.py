e65cf6e7702605939ab0a4ede86db3f728b1c4f7
aarushjoglekar <aarush.joglekar@gmail.com>
2026-07-17 15:51:30 -0700

    separate train & valid, truncate episode at num_steps, valid not counted toward num_steps, prep for multi-song

--- motiva/main.py
+++ motiva/main.py
@@ -47,137 +47,94 @@ def run_training(
 ):
     os.makedirs(model_path, exist_ok=True)
     num_steps = 0
-    episode = 0
-    next_validation = VALIDATION_INTERVAL
-    num_validations = 0
+    train_episode = 0
     f1_score_steps = []
     f1_scores = []
 
-    prev_target_num_states = 0
     total_train_time = 0
+    total_loop_time = 0
 
     train_data_path = os.path.join(model_path, "train_data.txt")
     if os.path.exists(train_data_path):
         with open(train_data_path) as f:
             header = f.readline()
 
-        episode = int(header.split("episode=")[1].split(" ")[0])
-        prev_target_num_states = int(header.split("target_num_steps=")[1].split(" ")[0])
-        num_steps = int(header.split("actual_num_steps=")[1].split(" ")[0])
-        total_train_time = float(header.split("total_train_time=")[1].strip())
-
-        next_validation = num_steps + VALIDATION_INTERVAL
+        train_episode = int(header.split("train_episodes=")[1].split(" ")[0])
+        num_steps = int(header.split("num_steps=")[1].split(" ")[0])
+        total_train_time = float(header.split("total_train_time=")[1].split(" ")[0])
+        total_loop_time = float(header.split("total_loop_time=")[1].strip())
 
         data = np.loadtxt(train_data_path)
         f1_score_steps = data[:, 0].tolist()
         f1_scores = data[:, 1].tolist()
-        print(f"Resumed with {len(f1_scores)} existing F1 scores")
+        print(f"Resumed with {len(f1_score_steps)} existing F1 scores")
+
+    next_validation = num_steps + VALIDATION_INTERVAL
+    total_target_steps = num_steps + NUM_STEPS
 
     start_time = time.perf_counter()
+    last_start_train_time = time.perf_counter()
+    train_checkpoint_time = 0
 
-    total_target_steps = NUM_STEPS + prev_target_num_states
+    while True:
+        validation_episode = num_steps >= next_validation
 
-    while num_steps < total_target_steps:
-        validation_episode = (num_steps >= next_validation) or (
-            num_steps + SONG.length >= total_target_steps
-        )
         if validation_episode:
+            train_checkpoint_time += time.perf_counter() - last_start_train_time
+
             next_validation += VALIDATION_INTERVAL
-            num_validations += 1
-
-        if validation_episode:
-            model.eval()
-        else:
-            model.train()
-
-        validation_midi_file = os.path.join(
-            model_path,
-            f"valid-{num_validations}.mid",
-        )
-        state = env.reset(
-            play_audio=False,
-            record_midi=validation_episode,
-            save_midi=(validation_episode and SAVE_TO_MIDI_VALID),
-            midi_file=validation_midi_file,
-        )
-        state = torch.from_numpy(state).float().to(device)
-
-        warmup_episode = False
-        sum_reward = 0
-        sum_actor_loss = torch.zeros((), device=device)
-        sum_critic_loss = torch.zeros((), device=device)
-        sum_log_prob = torch.zeros((), device=device)
-        sum_alpha = torch.zeros((), device=device)
-        steps = 0
-
-        episode_update_count = 0
-        episode_start_time = time.perf_counter()
-        while True:
-            action, _ = model.select_action(
-                state=state, deterministic=validation_episode
+            stats = run_validation_episode(
+                model=model,
+                env=env,
+                model_path=model_path,
+                f1_score_steps=f1_score_steps,
+                f1_scores=f1_scores,
+                num_steps=num_steps,
+                device=device,
             )
-            next_obs, reward, truncated = env.step(action=action.detach().cpu().numpy())
-            next_state = torch.from_numpy(next_obs).float().to(device)
 
-            if not validation_episode:
-                updated = model.update(
-                    state=state, next_state=next_state, action=action, reward=reward
-                )
-
-                if updated is not None:
-                    episode_update_count += model.updates_per_step
-
-                    actor_loss, critic_loss, log_probs, alpha = updated
-                    sum_actor_loss += actor_loss
-                    sum_critic_loss += critic_loss
-                    sum_log_prob += log_probs
-                    sum_alpha += alpha
-                else:
-                    warmup_episode = True
-
-            steps += 1
-            sum_reward += reward
-
-            state = next_state
-
-            if truncated:
-                break
-
-        episode += 1
-        num_steps += steps
-
-        episode_time = time.perf_counter() - episode_start_time
-
-        stats = None
-        if warmup_episode:
-            stats = "Warmup Episode: No Update Statistics"
-        elif validation_episode:
-            f1 = None
-            midi = env.save_piano_audio()
-            if midi is not None:
-                precision, recall, f1 = Song.from_midi(name="", midi=midi).compare_to(
-                    ground_truth=SONG
-                )
-                f1_score_steps.append(num_steps)
-                f1_scores.append(f1)
-            stats = f"Validation Episode - F1: {f1}, Precision: {precision}, Recall: {recall}"
+            last_start_train_time = time.perf_counter()
         else:
-            stats = f"Actor Loss: {(sum_actor_loss / steps).item()} || Critic Loss: {(sum_critic_loss / steps).item()} || Log Prob: {(sum_log_prob / steps).item()} || Alpha: {(sum_alpha / steps).item()} || Time/Update: {(round(1000 * episode_time / episode_update_count, 2))}ms"
+            train_episode += 1
+            stats, num_steps = run_train_episode(
+                model=model,
+                env=env,
+                max_steps=(total_target_steps - num_steps),
+                num_steps=num_steps,
+                episode=train_episode,
+                device=device,
+            )
 
-        print(
-            f"Episode: {episode} || Reward: {sum_reward} || {stats} || Total Steps: {num_steps}"
-        )
+        print(stats)
 
-    train_checkpoint_time = time.perf_counter() - start_time
-    total_train_time += train_checkpoint_time
-    print(
-        f"Train Checkpoint Time: {train_checkpoint_time / 3600} hrs || Total Train Time: {total_train_time / 3600} hrs"
+        if num_steps >= total_target_steps:
+            train_checkpoint_time += time.perf_counter() - last_start_train_time
+            break
+
+    stats = run_validation_episode(
+        model=model,
+        env=env,
+        model_path=model_path,
+        f1_score_steps=f1_score_steps,
+        f1_scores=f1_scores,
+        num_steps=num_steps,
+        device=device,
     )
+    print(stats)
+
+    loop_checkpoint_time = time.perf_counter() - start_time
+    total_train_time += train_checkpoint_time
+    total_loop_time += loop_checkpoint_time
+    print(f"Time Statistics:\n\
+            \tTrain Checkpoint Time: {train_checkpoint_time / 3600} hrs\n\
+            \tLoop Checkpoint Time: {loop_checkpoint_time / 3600} hrs\n\
+            \tTotal Train Time: {total_train_time / 3600} hrs\n\
+            \tTotal Loop Time: {total_loop_time / 3600} hrs")
 
     np.savetxt(
         train_data_path,
         np.column_stack([f1_score_steps, f1_scores]),
-        header=f"episode={episode} || target_num_steps={total_target_steps} || actual_num_steps={num_steps} || total_train_time={total_train_time}",
+        header=f"train_episodes={train_episode} || num_steps={num_steps} || total_train_time={total_train_time} || total_loop_time={total_loop_time}",
         fmt="%.6f",
     )
 
@@ -188,6 +145,121 @@ def run_training(
     plt.savefig(os.path.join(model_path, "f1-history.png"))
 
     model.save()
+
+
+def run_train_episode(model: SAC_DROQ, env: Environment, max_steps: int, num_steps: int, episode: int, device: str):
+    model.train()
+
+    state = env.reset(
+        play_audio=False,
+        record_midi=False,
+        save_midi=False,
+        midi_file="",
+    )
+    state = torch.from_numpy(state).float().to(device)
+
+    warmup_episode = False
+    sum_reward = 0
+    sum_actor_loss = torch.zeros((), device=device)
+    sum_critic_loss = torch.zeros((), device=device)
+    sum_log_prob = torch.zeros((), device=device)
+    sum_alpha = torch.zeros((), device=device)
+    steps = 0
+
+    episode_update_count = 0
+    episode_start_time = time.perf_counter()
+    while True:
+        action, _ = model.select_action(state=state, deterministic=False)
+        next_obs, reward, truncated = env.step(action=action.detach().cpu().numpy())
+        next_state = torch.from_numpy(next_obs).float().to(device)
+
+        updated = model.update(
+            state=state, next_state=next_state, action=action, reward=reward
+        )
+
+        if updated is not None:
+            episode_update_count += model.updates_per_step
+
+            actor_loss, critic_loss, log_probs, alpha = updated
+            sum_actor_loss += actor_loss
+            sum_critic_loss += critic_loss
+            sum_log_prob += log_probs
+            sum_alpha += alpha
+        else:
+            warmup_episode = True
+
+        steps += 1
+        sum_reward += reward
+
+        state = next_state
+
+        if truncated or steps >= max_steps:
+            break
+
+    episode_time = time.perf_counter() - episode_start_time
+
+    stats = f"Episode: {episode} || Reward: {sum_reward} || "
+    if warmup_episode:
+        stats += "Warmup Episode: No Update Statistics"
+    else:
+        stats += f"Actor Loss: {(sum_actor_loss / steps).item()} || Critic Loss: {(sum_critic_loss / steps).item()} || Log Prob: {(sum_log_prob / steps).item()} || Alpha: {(sum_alpha / steps).item()} || Time/Update: {(round(1000 * episode_time / episode_update_count, 2))}ms"
+    num_steps += steps
+    stats += f" || Total Steps: {num_steps}"
+
+    return stats, num_steps
+
+
+def run_validation_episode(
+    model: SAC_DROQ,
+    env: Environment,
+    model_path: str,
+    f1_score_steps: list[int],
+    f1_scores: list[float],
+    num_steps: int,
+    device: str,
+):
+    model.eval()
+
+    validation_midi_file = os.path.join(
+        model_path,
+        f"valid-{num_steps}.mid",
+    )
+    state = env.reset(
+        play_audio=False,
+        record_midi=True,
+        save_midi=SAVE_TO_MIDI_VALID,
+        midi_file=validation_midi_file,
+    )
+    state = torch.from_numpy(state).float().to(device)
+
+    sum_reward = 0
+
+    while True:
+        action, _ = model.select_action(state=state, deterministic=True)
+        next_obs, reward, truncated = env.step(action=action.detach().cpu().numpy())
+        next_state = torch.from_numpy(next_obs).float().to(device)
+        sum_reward += reward
+
+        state = next_state
+
+        if truncated:
+            break
+
+    f1 = None
+    precision = None
+    recall = None
+    midi = env.save_piano_audio()
+    if midi is not None:
+        precision, recall, f1 = Song.from_midi(name="", midi=midi).compare_to(
+            ground_truth=SONG
+        )
+
+        f1_score_steps.append(num_steps)
+        f1_scores.append(f1)
+
+    stats = f"Validation Episode - Reward: {sum_reward} F1: {f1}, Precision: {precision}, Recall: {recall}"
+
+    return stats
 
 
 def run_test(model: SAC_DROQ, env: Environment, model_path: str, device: str):
