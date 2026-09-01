14ed0ff61a9c812a23e956c9707a077ed9e950e3
aarushjoglekar <aarush.joglekar@gmail.com>
2026-08-31 23:09:11 -0700

    tune tendon max speeds

--- motiva/physicsenv/velocity_kp_tuner.py
+++ motiva/physicsenv/velocity_kp_tuner.py
@@ -69,7 +69,7 @@ def run_dashboard(joint_name, target_value, kp_value, data_queue, stop_event):
 
 
 def tune_velocity_kp(joint_name: str, is_left_hand: bool):
-    print("Tuning joint: ", joint_name)
+    print("Tuning joint:", joint_name)
     
     joint_index = None
     for index, actuated_joint in enumerate(constants.ACTUATED_JOINT_NAMES):
@@ -87,16 +87,36 @@ def tune_velocity_kp(joint_name: str, is_left_hand: bool):
         joint_index += physicsenv.num_actions() // 2
 
     hand_prefix = "lh" if is_left_hand else "rh"
-    joint_id = mujoco.mj_name2id(
-        physicsenv.model, mujoco.mjtObj.mjOBJ_JOINT, f"{hand_prefix}_{joint_name}"
+    actuator_id = mujoco.mj_name2id(
+        physicsenv.model, mujoco.mjtObj.mjOBJ_ACTUATOR, f"{hand_prefix}_A_{joint_name}"
     )
-    if joint_id == -1:
-        raise ValueError(f"Joint {hand_prefix}_{joint_name} does not exist")
+    if actuator_id == -1:
+        raise ValueError(f"Actuator {hand_prefix}_A_{joint_name} does not exist")
 
-    joint_qpos_adr = physicsenv.model.jnt_qposadr[joint_id]
-    joint_range_lo, joint_range_hi = physicsenv.model.jnt_range[joint_id]
-    joint_center = (joint_range_lo + joint_range_hi) / 2
-    joint_edge_margin = 0.05 * (joint_range_hi - joint_range_lo)
+    is_tendon = physicsenv.model.actuator_trntype[actuator_id] == mujoco.mjtTrn.mjTRN_TENDON
+    transmission_id = physicsenv.model.actuator_trnid[actuator_id, 0]
+
+    if is_tendon:
+        tendon_id = transmission_id
+        wrap_adr = physicsenv.model.tendon_adr[tendon_id]
+        wrap_num = physicsenv.model.tendon_num[tendon_id]
+        coupled_joint_ids = [
+            physicsenv.model.wrap_objid[i] for i in range(wrap_adr, wrap_adr + wrap_num)
+        ]
+
+        def read_joint_velocity():
+            return physicsenv.data.ten_velocity[tendon_id]
+    else:
+        coupled_joint_ids = [transmission_id]
+        joint_dof_adr = physicsenv.model.jnt_dofadr[transmission_id]
+
+        def read_joint_velocity():
+            return physicsenv.data.qvel[joint_dof_adr]
+
+    joint_qpos_adrs = [physicsenv.model.jnt_qposadr[jid] for jid in coupled_joint_ids]
+    joint_ranges = [physicsenv.model.jnt_range[jid] for jid in coupled_joint_ids]
+    joint_centers = [(lo + hi) / 2 for lo, hi in joint_ranges]
+    joint_edge_margins = [0.05 * (hi - lo) for lo, hi in joint_ranges]
 
     target_value = mp.Value("d", 0.0)
     kp_value = mp.Value("d", float(constants.ACTUATED_JOINT_KP_VELOCITY[joint_index]))
@@ -129,16 +149,19 @@ def tune_velocity_kp(joint_name: str, is_left_hand: bool):
             mujoco.mj_step(physicsenv.model, physicsenv.data)
         physicsenv.render()
 
-        joint_velocity = physicsenv.data.qvel[joint_id]
+        joint_velocity = read_joint_velocity()
         data_queue.put((step, target_velocity, joint_velocity))
 
         if CONTINUOUS_MOVEMENT:
-            joint_qpos = physicsenv.data.qpos[joint_qpos_adr]
-            if (
-                joint_qpos <= joint_range_lo + joint_edge_margin
-                or joint_qpos >= joint_range_hi - joint_edge_margin
+            recentered = False
+            for qpos_adr, (range_lo, range_hi), center, margin in zip(
+                joint_qpos_adrs, joint_ranges, joint_centers, joint_edge_margins
             ):
-                physicsenv.data.qpos[joint_qpos_adr] = joint_center
+                qpos = physicsenv.data.qpos[qpos_adr]
+                if qpos <= range_lo + margin or qpos >= range_hi - margin:
+                    physicsenv.data.qpos[qpos_adr] = center
+                    recentered = True
+            if recentered:
                 mujoco.mj_forward(physicsenv.model, physicsenv.data)
 
     dashboard_process.terminate()
